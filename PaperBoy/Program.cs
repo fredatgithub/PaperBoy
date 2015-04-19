007d782c63053645ed6e6eaaf69202e0739f1520
Freddy Juhel <domaine75@gmail.com>
2015-04-19 02:01:15 +0200

    Remove unused method

--- PaperBoy/Program.cs
+++ PaperBoy/Program.cs
@@ -78,35 +78,5 @@ namespace PaperBoy
 
       return result;
     }
-
-    private static string GetWebClientString(string url = "http://www.google.com/")
-    {
-      // create a new instance of WebClient
-      WebClient client = new WebClient();
-      string result = string.Empty;
-      // set the user agent to IE6
-      client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; .NET CLR 1.0.3705;)");
-      try
-      {
-        // actually execute the GET request
-        string ret = client.DownloadString(url);
-
-        // ret now contains the contents of the webpage
-        Console.WriteLine("First 256 bytes of response: " + ret.Substring(0, 265));
-        result = ret;
-      }
-      catch (WebException we)
-      {
-        // WebException.Status holds useful information
-        Console.WriteLine(we.Message + "\n" + we.Status.ToString());
-      }
-      catch (NotSupportedException ne)
-      {
-        // other errors
-        Console.WriteLine(ne.Message);
-      }
-
-      return result;
-    }
   }
 }
