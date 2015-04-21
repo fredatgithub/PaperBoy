f9d7979427e7bdd0a5cb7eaa6b8d43a5e2048965
Freddy Juhel <domaine75@gmail.com>
2015-04-21 17:10:59 +0200

    Add Newspaper class

---
+++ ManualPaperBoy/Newspaper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ManualPaperBoy
+{
+  public class Newspaper
+  {
+    public string Name { get; set; }
+    public string Url { get; set; }
+    public DateTime PublishDate { get; set; }
+    public bool IsPrintedOnWeekEnd { get; set; }
+    public DateTime FirstPrintDate { get; set; }
+
+    public Newspaper(string name, string url, DateTime publishDate, bool isPrintedonWeekEnd = false)
+    {
+      Name = name;
+      Url = url;
+      PublishDate = publishDate;
+      IsPrintedOnWeekEnd = IsPrintedOnWeekEnd;
+    }
+  }
+}
